4747ff731a8ef048899dc7498b5ee2efb9c872da
logstar <y.will.zhang@gmail.com>
2018-03-21 02:15:42 -0400

    Add iterhac.MultinomialMdl and HClustTree
    HClustTree is a wrapper of scipy.cluster.hierarchy.ClusterNode, offering
    simple tree operation helper functions.
    
    MultinomialMdl computes MDL using multinomial distribution.

--- scxplit/iterhac.py
+++ scxplit/iterhac.py
@@ -1,2 +1,208 @@
 import numpy as np
-import multiprocessing as mp
+
+import scipy.cluster.hierarchy as sph
+import scipy.spatial as sps
+
+from . import utils
+from . import eda
+
+class MultinomialMdl(object):
+    """
+    Encode discrete values using multinomial distribution
+    Input:
+    x: 1d array. Should be non-negative
+    
+    Notes:
+    When x only has 1 uniq value. Encode the value and 
+    the number of values by 32bits float.
+    """
+    def __init__(self, x):
+        super(MultinomialMdl, self).__init__()
+        x = np.array(x)
+        if x.ndim != 1:
+            raise ValueError("x should be 1D array. "
+                             "x.shape: {}".format(x.shape))
+        self._x = x
+        self._n = x.shape[0]
+        self._mdl = self._mn_mdl()
+        return
+    
+    def _mn_mdl(self):
+        uniq_vals, uniq_val_cnts = np.unique(self._x, return_counts=True)
+        if len(uniq_vals) > 1:
+            return (-np.log(uniq_val_cnts / uniq_val_cnts.sum()) * uniq_val_cnts).sum()
+        elif len(uniq_vals) == 1:
+            return np.log(uniq_val_cnts) + 1
+        else:
+            # len(x) == 0
+            return 0
+
+    @property
+    def x(self):
+        return self._x.tolist()
+
+    @property
+    def mdl(self):
+        return self._mdl
+
+
+class HClustTree(object):
+    """
+    Hierarchical clustering tree. Implement simple tree operation routines. 
+    HCT is binary unbalanced tree. 
+    Attributes:
+    node : scipy.cluster.hierarchy.ClusterNode
+        current node
+    prev : HClustTree
+        parent of current node
+    """
+    def __init__(self, node, prev=None):
+        super(HClustTree, self).__init__()
+        self._node = node
+        self._prev = prev
+
+        if node is None:
+            left = None
+            right = None
+        else:
+            left = node.get_left()
+            right = node.get_right()
+
+        self._left = left
+        self._right = right
+
+    @property
+    def prev(self):
+        return self._prev
+
+    def count(self):
+        if self._node is None:
+            return 0
+        else:
+            return self._node.get_count()
+    
+    def left(self):
+        return HClustTree(self._left, self)
+
+    def left_count(self):
+        return self.left().count()
+
+    def right(self):
+        return HClustTree(self._right, self)
+
+    def right_count(self):
+        return self.right().count()
+    
+    def leaf_ids(self):
+        if self._node is None:
+            return None
+        else:
+            return self._node.pre_order(lambda xn: xn.get_id())
+
+    def left_leaf_ids(self):
+        return self.left().leaf_ids()
+
+    def right_leaf_ids(self):
+        return self.right().leaf_ids()
+
+    def bi_partition(self, return_subtrees=False):
+        clabs, cids = self.cluster_id_list_to_clab_array([self.left_leaf_ids(), 
+                                                          self.right_leaf_ids()],
+                                                         self.leaf_ids())
+        if return_subtrees:
+            return clabs, cids, self.left(), self.right()
+        else:
+            return clabs, cids
+
+    def n_round_bipar_cnt(self, n):
+        assert n > 0
+        nr_bipar_cnt_list = []
+        curr_hct_list = [self]
+        curr_hct_cnt_list = []
+        next_hct_list = []
+        for i in range(n):
+            for iter_hct in curr_hct_list:
+                iter_left = iter_hct.left()
+                iter_right = iter_hct.right()
+                next_hct_list += [iter_left, iter_right]
+                curr_hct_cnt_list += [iter_left.count(), iter_right.count()]
+            nr_bipar_cnt_list.append(curr_hct_cnt_list)
+
+            curr_hct_list = next_hct_list
+            next_hct_list = []
+            curr_hct_cnt_list = []
+        return nr_bipar_cnt_list
+
+    @staticmethod
+    def cluster_id_list_to_clab_array(cl_id_list, id_list):
+        # convert [[0, 1, 2], [3,4]] to np.array([0, 0, 0, 1, 1])
+        # according to id_arr [0, 1, 2, 3, 4]
+
+        # checks uniqueness
+        eda.SampleFeatureMatrix.check_is_valid_sfids(id_list)
+
+        if type(cl_id_list) != list:
+            raise ValueError("cl_id_list must be a list: {}".format(cl_id_list))
+
+        for x in cl_id_list:
+            eda.SampleFeatureMatrix.check_is_valid_sfids(x)
+
+        cl_id_mlist = np.concatenate(cl_id_list).tolist()
+        eda.SampleFeatureMatrix.check_is_valid_sfids(cl_id_mlist)
+
+        # np.unique returns sorted unique values
+        if np.all(sorted(id_list) != sorted(cl_id_mlist)):
+            raise ValueError("id_list should have the same ids as cl_id_list.")
+
+        cl_id_dict = {}
+        # iter_cl_ind : cluster index
+        # iter_cl_ids: individual cluster list
+        for iter_cl_ind, iter_cl_ids in enumerate(cl_id_list):
+            assert len(iter_cl_ids) > 0
+            for cid in iter_cl_ids:
+                cl_id_dict[cid] = iter_cl_ind
+
+        clab_list = [cl_id_dict[x] for x in id_list]
+        return clab_list, id_list
+
+    @staticmethod
+    def hclust_tree(dmat, linkage="complete", n_eval_rounds = None, 
+                    is_euc_dist=False, verbose = False):
+        dmat = np.array(dmat, dtype="float")
+        dmat = eda.SampleDistanceMatrix.num_correct_dist_mat(dmat)
+        
+        n = dmat.shape[0]
+
+        if linkage == "auto":
+            try_linkages = ("single", "complete", "average", "weighted")
+            
+            if is_euc_dist:
+                try_linkages += ("centroid", "median", "ward")
+
+            if n_eval_rounds is None:
+                n_eval_rounds = int(np.ceil(np.log2(n)))
+            else:
+                n_eval_rounds = int(np.ceil(max(np.log2(n), n_eval_rounds)))
+
+            ltype_mdl_list = []
+            for iter_ltype in try_linkages:
+                iter_lhct = HClustTree.hclust_tree(dmat, linkage = iter_ltype)
+                iter_nbp_cnt_list = iter_lhct.n_round_bipar_cnt(n_eval_rounds)
+                iter_nbp_mdl_arr = np.array(list(map(
+                    lambda x: MultinomialMdl(np.array(x)).mdl,
+                    iter_nbp_cnt_list)))
+                iter_nbp_mdl = np.sum(iter_nbp_mdl_arr 
+                                      / np.arange(1, n_eval_rounds + 1))
+                ltype_mdl_list.append(iter_nbp_mdl)
+
+            linkage = try_linkages[ltype_mdl_list.index(max(ltype_mdl_list))]
+
+            if verbose:
+                print(linkage, tuple(zip(try_linkages, ltype_mdl_list)),
+                      sep = "\n")
+
+        dmat_sf = sps.distance.squareform(dmat)
+        hac_z = sph.linkage(dmat_sf, method=linkage, optimal_ordering=False)
+        return HClustTree(sph.to_tree(hac_z))
+
+
---
+++ tests/test_iterhac.py
@@ -0,0 +1,109 @@
+import pytest
+
+import numpy as np
+
+import scxplit.iterhac as ihac
+import scxplit.eda as eda
+
+class TestMultinomialMdl(object):
+    """docstring for TestMultinomialMdl"""
+    def test_empty_x(self):
+        mmdl = ihac.MultinomialMdl([])
+        assert mmdl.mdl == 0
+
+    def test_single_level(self):
+        mmdl = ihac.MultinomialMdl(["a"]*10)
+        np.testing.assert_allclose(mmdl.mdl, np.log(10) + 1)
+
+    def test_multi_levels(self):
+        x = ["a"]*10 + ["b"]*25
+        ux, uxcnt = np.unique(x, return_counts=True)
+        mmdl = ihac.MultinomialMdl(x)
+        np.testing.assert_allclose(mmdl.mdl, 
+            (-np.log(uxcnt / len(x)) * uxcnt).sum())
+
+    def test_wrong_x_shape(self):
+        with pytest.raises(ValueError) as excinfo:
+            ihac.MultinomialMdl(np.arange(6).reshape(3, 2))
+
+    def test_getter(self):
+        mmdl = ihac.MultinomialMdl([])
+        assert mmdl.x == []
+        mmdl2 = ihac.MultinomialMdl([0, 0, 1, 1, 1])
+        assert mmdl2.x == [0, 0, 1, 1, 1]
+
+class TestHClustTree(object):
+    """docstring for TestHClustTree"""
+    sdm_5x2 = eda.SampleDistanceMatrix([[0,0], 
+                                        [100, 100], 
+                                        [1,1], 
+                                        [101, 101], 
+                                        [80, 80]],
+                                        metric="euclidean")
+
+    hct = ihac.HClustTree.hclust_tree(sdm_5x2.d, linkage="auto")
+    
+    def test_hclust_tree_args(self):
+        ihac.HClustTree.hclust_tree(self.sdm_5x2.d, linkage="auto",
+                                    n_eval_rounds=-1, is_euc_dist=True, 
+                                    verbose=True)
+
+    def test_hclust_tree(self):
+        assert self.hct.prev is None
+
+        assert self.hct.left_count() == 2
+        assert self.hct.right_count() == 3
+        assert self.hct.count() == 5
+
+        assert len(self.hct.leaf_ids()) == 5
+        assert np.all(np.in1d(self.hct.leaf_ids(), [0, 1, 2, 3, 4]))
+
+        assert len(self.hct.left_leaf_ids()) == 2
+        assert self.hct.left_leaf_ids() == [0, 2]
+
+        assert len(self.hct.right_leaf_ids()) == 3
+        assert self.hct.right_leaf_ids() == [4, 1, 3]
+
+        assert self.hct.left().left().left().count() == 0
+        assert self.hct.left().left().left().leaf_ids() is None
+        assert self.hct.left().left().left_leaf_ids() is None
+        assert self.hct.left().left().right().count() == 0
+        
+    def test_hclust_tree_invalid_dmat(self):
+        with pytest.raises(ValueError) as excinfo:
+            ihac.HClustTree.hclust_tree(np.arange(5))
+
+        with pytest.raises(ValueError) as excinfo:
+            ihac.HClustTree.hclust_tree(np.arange(10).reshape(2, 5))
+
+    def test_bi_partition(self):
+        # return subtrees False
+        clabs1, cids1 = self.hct.bi_partition()
+
+        # return subtrees True
+        clasb2, cids2, lst, rst = self.hct.bi_partition(return_subtrees=True)
+
+        assert clabs1 == clasb2
+        assert cids1 == cids2
+
+        assert lst.count() == 2
+        assert lst.left_count() == 1
+        assert lst.left_leaf_ids() == [0]
+        assert lst.right_leaf_ids() == [2]
+        assert lst.leaf_ids() == [0, 2]
+
+        assert rst.leaf_ids() == [4, 1, 3]
+        assert rst.right_leaf_ids() == [1, 3]
+        assert rst.left_leaf_ids() == [4]
+
+    def test_cluster_id_list_to_clab_array_wrong_id_list_type(self):
+        with pytest.raises(ValueError) as excinfo:
+            ihac.HClustTree.cluster_id_list_to_clab_array(
+                np.array([[0, 1, 2], [3,4]]), [0, 1, 2, 3, 4])
+
+    def test_cluster_id_list_to_clab_array_mismatched_ids_cids(self):
+        with pytest.raises(ValueError) as excinfo:
+            ihac.HClustTree.cluster_id_list_to_clab_array(
+                [[0, 1, 2], [3,4]], [0, 1, 2, 3, 5])
+
+
