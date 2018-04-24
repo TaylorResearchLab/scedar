a8400829e05ddf329f54f58bfa3653e5b35d29e7
logstar <y.will.zhang@gmail.com>
2018-04-24 19:51:52 -0400

    Add shell scripts to run tests

---
+++ scripts/gen_baseline_images.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+source activate py36
+python setup.py test \
+  --addopts "--mpl-generate-path=tests/baseline_images \
+             --color=yes --cov-config .coveragerc \
+             --cov-branch --cov=scedar \
+             --ignore=tests/test_cluster/test_mirac_large_data.py\"
