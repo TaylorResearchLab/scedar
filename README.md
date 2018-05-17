c4b96ac46086dfde00c12ab9184631c08ef245a5
logstar <y.will.zhang@gmail.com>
2018-05-17 17:00:50 -0400

    Fix scripts/gen_baseline_images.sh
    Re-generated images with Python3.5.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Single-cell explorative data analysis for RNA-Seq
 
+[![Build Status](https://travis-ci.com/logstar/scedar.svg?token=VYpRBjS777dyXHuzCsTN&branch=master)](https://travis-ci.com/logstar/scedar)
+
 TBC. 
--- scripts/gen_baseline_images.sh
+++ scripts/gen_baseline_images.sh
@@ -3,4 +3,4 @@ python setup.py test \
   --addopts "--mpl-generate-path=tests/baseline_images \
              --color=yes --cov-config .coveragerc \
              --cov-branch --cov=scedar \
-             --ignore=tests/test_cluster/test_mirac_large_data.py\"
+             --ignore=tests/test_cluster/test_mirac_large_data.py"
