7636c6e59d78dc45baf6778194933bbafc827086
ben stear <bjs385@drexel.edu>
2020-10-01 15:50:14 -0400

    Update __init__.py

--- scedar/__init__.py
+++ scedar/__init__.py
@@ -2,5 +2,6 @@ from scedar import eda
 from scedar import cluster
 from scedar import utils
 from scedar import knn
+from scedar import qc
 
-__all__ = ["eda", "cluster", "knn", "utils"]
+__all__ = ["eda", "cluster", "knn","qualitycontrol", "utils"]
