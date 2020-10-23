ccafcc026d073c45e81e00c2f61ac3fbb2a91153
ben stear <bjs385@drexel.edu>
2020-10-23 10:08:29 -0400

    Update pytest_all.sh

--- scripts/pytest_all.sh
+++ scripts/pytest_all.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-pytest --mpl --mpl-baseline-path=tests/baseline_images \
-       --color=yes --cov-config .coveragerc --cov-branch --cov=scedar \
-       tests
+
+# GITHUB Actions version of pytest_all
+coverage run --source=scedar -m pytest --mpl \
+     --mpl-baseline-path=tests/baseline_images --color=yes tests
+
+# TRAVIS version of pytest_all
+#pytest --mpl --mpl-baseline-path=tests/baseline_images \
+#       --color=yes --cov-config .coveragerc --cov-branch --cov=scedar \
+#       tests
