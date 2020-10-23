134aa04bdc5235602439cacd060db36b7c252435
ben stear <bjs385@drexel.edu>
2020-10-23 10:12:20 -0400

    Update gen_baseline_images.sh

--- scripts/gen_baseline_images.sh
+++ scripts/gen_baseline_images.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-pytest --mpl-generate-path=tests/baseline_images \
-       --color=yes --cov-config .coveragerc \
-       --cov-branch --cov=scedar \
-       --ignore=tests/test_cluster/test_mirac_large_data.py \
-       tests
+
+# GITHUB Actions version of gen_baseline_images.sh (using coverage as an intermediate between pytest and Coveralls)
+coverage run --source=scedar -m pytest --mpl-generate-path=tests/baseline_images \
+   --color=yes --ignore=tests/test_cluster/test_mirac_large_data.py tests
+
+# TRAVIS version of gen_baseline_images.sh
+#pytest --mpl-generate-path=tests/baseline_images \
+#       --color=yes --cov-config .coveragerc \
+#       --cov-branch --cov=scedar \
+#       --ignore=tests/test_cluster/test_mirac_large_data.py \
+#       tests
